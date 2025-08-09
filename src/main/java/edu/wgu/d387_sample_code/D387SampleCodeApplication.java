3a441f1ea5af1d589fdaf4a0350db165b3af06dd
rymann22 <64804167+rymann22@users.noreply.github.com>
2025-08-09 12:45:48 -0600

    updated D387SampleCodeApplication.java file

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -3,13 +3,35 @@ package edu.wgu.d387_sample_code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+		executorService.execute(() -> {
+			Locale enLocale = new Locale("en", "US");
+			ResourceBundle enBundle = ResourceBundle.getBundle("welcome", enLocale);
+			System.out.println(enBundle.getString("welcome"));
+		});
+
+		executorService.execute(() -> {
+			Locale frLocale = new Locale("fr", "CA");
+			ResourceBundle frBundle = ResourceBundle.getBundle("welcome", frLocale);
+			System.out.println(frBundle.getString("welcome"));
+		});
+
+		executorService.shutdown();
 	}
 
 }
