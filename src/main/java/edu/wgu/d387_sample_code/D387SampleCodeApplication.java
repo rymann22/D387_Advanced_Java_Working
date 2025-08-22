2937df9f67e93aa9a18fb2c4aaf3b2d79aa6ab36
rymann22 <64804167+rymann22@users.noreply.github.com>
2025-08-22 15:26:31 -0600

    [ 005 - Welcome message back-end ] State - working Files ------------------------- D387SampleCodeApplication

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -4,12 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import edu.wgu.d387_sample_code.services.WelcomeMessage;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.Locale;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
-	}
 
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		executor.execute(() -> {
+			String msg = WelcomeMessage.getWelcomeMessage(new Locale("en", "US"));
+			System.out.println(msg);
+		});
+
+		executor.execute(() -> {
+			String msg = WelcomeMessage.getWelcomeMessage(new Locale("fr", "CA"));
+			System.out.println(msg);
+		});
+
+		executor.shutdown();
+	}
 }
